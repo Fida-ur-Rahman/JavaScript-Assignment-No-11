edd1aff7dd1586ac690655e217b02fe24345a4e3
Fida-ur-Rahman <fidasaba15@gmail.com>
2025-12-28 23:15:17 +0500

    JavaScript Assignment No 11

---
+++ array.js
@@ -0,0 +1,9 @@
+// ARRAY IN JAVASCRIPT
+let marks = [34, 56, 67, 78, 89, 12, 34];
+console.log(marks)
+console.log(marks.length)
+
+let Student = ["Fida", "Sami", "Rahman"];
+for(let idx = 0; idx < marks.length; idx++){
+    console.log(Student[idx])
+}
